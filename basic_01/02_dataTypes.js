a9f1bf342ae7c38d2b7d94745ca921000556be57
Aiman Waseem <155463719+a-aiman25@users.noreply.github.com>
2026-03-29 17:06:41 +0000

    container config done

---
+++ basic_01/02_dataTypes.js
@@ -0,0 +1,13 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs not browser
+
+let name = "Aiman"       // String
+let age = 22             // Number/Integer
+let isLoggedIn = false   // Boolean
+let space = null         // Empty value (Standalone value)
+let state;               // undefined
+
+console.log(typeof undefined); // undefined
+console.log(typeof null);      // Object
+
